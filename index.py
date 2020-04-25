b95573fb643f09a75febdaae827365ce09f7892c
ModgarLar <alberto.lara.specialisterne@gmail.com>
2020-04-25 20:00:56 +0200

    Added an executable

--- index.py
+++ index.py
@@ -1,31 +1,92 @@
 import pandas as pd
+import math
 import json
 from datetime import datetime
 
-cols = [0,1,2,3]
-wb = pd.read_excel('Horario.xlsx',sheet_name='Horario', encoding='utf8')
-data = wb.to_dict(orient='record')
-rows = len(wb)
-cols = 4
-obj = {}
-for i in range(rows):
-    vals = data[i]
-    dia = vals['Dia']
-    day = dia.strftime('%d/%m/%y')
-    if day not in obj.keys():
+
+def getSheet(sheet_name):
+    wb = pd.read_excel('Horario.xlsx', sheet_name=sheet_name)
+    datos = wb.to_dict(orient='record')
+    return datos
+
+def getSheetData(sheet_name, datos):
+    rows = len(datos)
+    obj = {}
+    for i in range(rows):
+        vals = list(datos[i].values())
+        dia = vals[0]
+        day = dia.strftime('%d/%m/%y')
+        if sheet_name == 'Horario':
+            if day not in obj.keys():
+                obj[day] = {}
+            task = vals[1]
+            if task not in obj[day].keys():
+                obj[day][task] = 0
+            init = vals[2]
+            fin = vals[3]
+            date1 = datetime.combine(dia, fin)
+            date2 = datetime.combine(dia, init)
+            time = (date1.minute + date1.hour*60)*60 - (date2.minute + date2.hour*60)*60
+            obj[day][task] += int(time / 60)
+        elif sheet_name == 'Imputadas':
+            time = vals[1]
+            obj[day]=time
+
+    return obj
+
+def getArray(obj):
+    data = []
+    for day in obj.keys():
+        tasks = obj[day]
+        for task in tasks.keys():
+            time = tasks[task]
+            data.append([day, task, time])
+    return data
+
+
+def getHours(data,input):
+    days = input.keys()
+    obj = {}
+    acum = 0
+    for day in days:
+        tasks = list(data[day].keys())
+        if 'Comida' in tasks:
+            data[day].pop('Comida')
+        real = sum(list(data[day].values()))
+        inputs = input[day]*60
+        dif = real - inputs
+        acum += dif
+        obj[day] = {'real':real,
+                    'input':inputs,
+                    'dif':dif,
+                    'acum':acum}
+    return obj
+
+
+def getTickets(data):
+    obj = {}
+    for day in data.keys():
         obj[day] = {}
-    task = vals['Tarea']
-    if task not in obj[day].keys():
-        obj[day][task] = 0
-    init = vals['Hora Inicio']
-    fin = vals['Hora Final']
-    time = datetime.combine(dia,fin) - datetime.combine(dia,init)
-    obj[day][task] += int(time.seconds/60)
+        tasks = data[day]
+        for task in tasks.keys():
+            if '-' in task:
+                project = task.split('-')[0]
+                time =tasks[task]
+                if project not in obj[day].keys():
+                    obj[day][project] = {}
 
-data = []
+                if task not in obj[day][project].keys():
+                    obj[day][project][task] = time
 
-for day in obj.keys():
-    tasks = obj[day]
-    for task in tasks.keys():
-        time = tasks[task]
-        data.append([day,task,time])
+    return obj
+
+
+datos1 = getSheet('Horario')
+datos2 = getSheet('Imputadas')
+
+
+data1 = getSheetData('Horario',datos1)
+data2 = getSheetData('Imputadas',datos2)
+getHours(data1,data2)
+
+tickets = getTickets(data1)
