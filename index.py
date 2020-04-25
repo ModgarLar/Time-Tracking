3c0e2ec9f129477474975b54623d4c94d7e5df32
ModgarLar <alberto.lara.specialisterne@gmail.com>
2020-04-25 02:36:49 +0200

    First Commit

--- index.py
+++ index.py
@@ -1,4 +1,31 @@
-from openpyxl import load_workbook
+import pandas as pd
+import json
+from datetime import datetime
 
+cols = [0,1,2,3]
+wb = pd.read_excel('Horario.xlsx',sheet_name='Horario', encoding='utf8')
+data = wb.to_dict(orient='record')
+rows = len(wb)
+cols = 4
+obj = {}
+for i in range(rows):
+    vals = data[i]
+    dia = vals['Dia']
+    day = dia.strftime('%d/%m/%y')
+    if day not in obj.keys():
+        obj[day] = {}
+    task = vals['Tarea']
+    if task not in obj[day].keys():
+        obj[day][task] = 0
+    init = vals['Hora Inicio']
+    fin = vals['Hora Final']
+    time = datetime.combine(dia,fin) - datetime.combine(dia,init)
+    obj[day][task] += int(time.seconds/60)
 
-workbook = load_workbook(filename="Horario.xlsx")
+data = []
+
+for day in obj.keys():
+    tasks = obj[day]
+    for task in tasks.keys():
+        time = tasks[task]
+        data.append([day,task,time])
